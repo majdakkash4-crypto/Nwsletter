91bbb6d52c0c55afdb63b44f6435469bfba4b725
majdakkash4-crypto <majdakkash4@gmail.com>
2026-03-29 23:33:20 +0200

    Update send.js

--- api/send.js
+++ api/send.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -35,4 +35,4 @@ export default async function handler(req, res) {
     console.error('Handler error:', err);
     return res.status(500).json({ error: err.message });
   }
-}
+};
